859a7036f5dce43935e589776c612fda4153a1f1
wuxiaopeng <1099272020@qq.com>
2018-12-20 16:07:04 +0800

    C++项目提交

---
+++ studyC++/src/studyC++.cpp
@@ -0,0 +1,24 @@
+//============================================================================
+// Name        : studyC++.cpp
+// Author      : wuxiaopeng
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	char *s = "Golden Global View";
+
+	memset(s, 'G', 6);
+
+	printf("%s", s);
+
+	getchar();
+	return 0;
+}
+
